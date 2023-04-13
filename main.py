3c3a30fdf08314c459e39da53e212f2b928c932c
ajwallacemusic@gmail.com <ajwallacemusic@gmail.com>
2023-04-13 09:54:10 -0500

    add search template, update main code to parse better and point to local es

--- main.py
+++ main.py
@@ -3,23 +3,11 @@ from elasticsearch import Elasticsearch
 from txtai.pipeline import Similarity
 import streamlit as st
 
-# Create similarity instance for re-ranking
-similarity = Similarity("valhalla/distilbart-mnli-12-3", crossencode=True)
-
-def ranksearch(query, limit):
-  results = [text for _, text in search(query, limit * 10)]
-  return [(score, results[x]) for x, score in similarity(query, results)][:limit]
-
 # Connect to ES instance
-es = Elasticsearch(hosts=["<url>"], request_timeout=60, retry_on_timeout=True)
+es = Elasticsearch(hosts=["http://localhost:9200"], request_timeout=60, retry_on_timeout=True)
 
-def table(category, query, rows):
-    st.write(category, query)
-
-    for score, text in rows:
-        st.write(score, text)
-
-
+# Create similarity instance for re-ranking
+similarity = Similarity("valhalla/distilbart-mnli-12-3")
 
 def search(search_term, limit):
   body = {
@@ -41,22 +29,41 @@ def search(search_term, limit):
 }
   
   results = []
-  for result in es.search_template(index="assets", body=body)["hits"]["hits"]:
+  for result in es.search_template(index="movies", body=body)["hits"]["hits"]:
     source = result["_source"]
-    text = source["name"] + "\n" + source["description"]
-    results.append((result["_score"], text))
+    text = source["title"] + "\n" + source["description"]
+    results.append({"score":result["_score"], "doc":{"title": source["title"], "description": source["description"]}})
   return results
 
+def ranksearch(query, limit):
+  es_results = search(query, limit * 10)
+  text = []
+  for result in es_results:
+    text.append(result["doc"]["title"] + "\n" + result["doc"]["description"])
+  return [(score, text[x]) for x, score in similarity(query, text)][:limit]
 
 def main():
-    st.title("Semantic Search")
+    st.title("Semantic Movie Search")
     # User search   
     user_input = st.text_area("Search box", "toys come alive")
     limit = 50
+    es_results = search(user_input, limit)
+    txtai_results = ranksearch(user_input, limit)
 
-
-    table("Elasticsearch", user_input, search(user_input, limit))
-    table("Elasticsearch + txtai", user_input, ranksearch(user_input, limit))
+    col1, col2, = st.columns(2)
+    with col1:
+       st.header("Elasticsearch Results")
+       for result in es_results:
+        st.write(result["score"])
+        st.text(result["doc"]["title"])
+        st.write(result["doc"]["description"])
+    with col2:
+       st.header("txtai")
+       for score, text in txtai_results:
+          st.write(score)
+          txtArr = text.split("\n")
+          st.text(txtArr[0])
+          st.write(txtArr[1])
 
 if __name__ == "__main__":
    main()
